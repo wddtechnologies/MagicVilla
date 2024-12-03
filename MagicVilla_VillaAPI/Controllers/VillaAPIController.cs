82dd3757bd3996484733b5644a6cc05af04a5554
Louise <wddtechnologies@users.noreply.github.com>
2024-12-03 11:10:28 -0500

    Part 2 added Post, Update, Delete HTTP

---
+++ MagicVilla_VillaAPI/Controllers/VillaAPIController.cs
@@ -0,0 +1,96 @@
+﻿using MagicVilla_VillaAPI.Data;
+using MagicVilla_VillaAPI.Models;
+using MagicVilla_VillaAPI.Models.Dto;
+using Microsoft.AspNetCore.Mvc;
+namespace MagicVilla_VillaAPI.Controllers
+{
+    [Route("api/VillaAPI")]
+    [ApiController]
+    public class VillaAPIController : ControllerBase
+    {
+        [HttpGet]
+        public ActionResult<IEnumerable<VilliaDTO>> GetVillas()
+        {
+            return Ok(VillaStore.villaList);
+        }
+        [HttpGet("{id:int}", Name = "GetVilla")]
+        [ProducesResponseType(200)]
+        [ProducesResponseType(404)]
+        [ProducesResponseType(400)]
+        public ActionResult<VilliaDTO> GetVilla(int id)
+        {
+            if (id == 0)
+            {
+                return BadRequest();
+            }
+            var villa = VillaStore.villaList.FirstOrDefault(u => u.Id == id);
+            if (villa == null) {
+                return NotFound();
+            }
+            return Ok();
+        }
+        [HttpPost]
+        public ActionResult<VilliaDTO> CreateVilla([FromBody] VilliaDTO villaDTO)
+        {
+            // Check if the villaDTO is null
+            if (villaDTO == null)
+            {
+                return BadRequest(villaDTO);
+            }
+
+            // Check if a villa with the same name already exists
+            //used StringComparison.OrdinalIgnoreCase instead of lower case
+            if (VillaStore.villaList.FirstOrDefault(u => u.Name.Equals(villaDTO.Name, StringComparison.OrdinalIgnoreCase)) != null)
+            {
+                ModelState.AddModelError("CustomError", "Villa already exists");
+                return BadRequest(ModelState);
+            }
+
+            // Validate that the ID is not set
+            if (villaDTO.Id > 0)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError);
+            }
+
+            // Generate a new ID and add the villa to the list
+            villaDTO.Id = VillaStore.villaList.OrderByDescending(u => u.Id).FirstOrDefault()?.Id + 1 ?? 1;
+            VillaStore.villaList.Add(villaDTO);
+
+            // Return a CreatedAtRoute response with the new villa details
+            return CreatedAtRoute("GetVilla", new { id = villaDTO.Id }, villaDTO);
+        }
+
+        [HttpDelete("{id:int}", Name = "DeleteVilla")]
+        public ActionResult<VilliaDTO> DeleteVilla(int id)
+        {
+            if (id == 0)
+            {
+                return BadRequest();
+            }
+            var villa = VillaStore.villaList.FirstOrDefault(u => u.Id == id);
+            if (villa == null) 
+            {
+                return NotFound();
+            }
+            VillaStore.villaList.Remove(villa);
+            return NoContent();
+        }
+        [HttpPut("{id:int}", Name = "UpdateVilla")]
+        public ActionResult UpdateVilla(int id, [FromBody] VilliaDTO villiaDTO)
+        {
+            if (villiaDTO == null || id != villiaDTO.Id)
+            {
+                return BadRequest();
+            }
+            var villa = VillaStore.villaList.FirstOrDefault(u =>u.Id == id);
+            villa.Name = villiaDTO.Name;
+            villa.Sqft = villiaDTO.Sqft;
+            villa.Occupancy = villiaDTO.Occupancy;
+
+            return NoContent ();
+
+
+
+        }
+    }
+}
---
+++ MagicVilla_VillaAPI/Data/VillaStore.cs
@@ -0,0 +1,15 @@
+﻿using MagicVilla_VillaAPI.Models.Dto;
+
+namespace MagicVilla_VillaAPI.Data
+{
+    public static class VillaStore
+    {
+        public static List<VilliaDTO> villaList = new List<VilliaDTO>()
+        {
+                new VilliaDTO {Id=1, Name="Pool side View",Sqft=150,Occupancy=4 },
+                new VilliaDTO {Id=2, Name="Beach side View",Sqft=100, Occupancy =3},
+                new VilliaDTO {Id=3, Name="Window side View",Sqft=90,Occupancy=2},
+                new VilliaDTO {Id=4, Name="Shop side View",Sqft=200,Occupancy=6}
+        };
+    }
+}
