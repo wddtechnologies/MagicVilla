330cdf3efa304af4fd22735e4f7a7bee1b1e1311
Louise <wddtechnologies@users.noreply.github.com>
2024-12-06 10:09:56 -0500

    Updated the controller Day 5 everything works for the API

--- MagicVilla_VillaAPI/Controllers/VillaAPIController.cs
+++ MagicVilla_VillaAPI/Controllers/VillaAPIController.cs
@@ -14,13 +14,10 @@ namespace MagicVilla_VillaAPI.Controllers
     {
         private readonly ApplicationDbContext _db;
         
-
         public VillaAPIController(ApplicationDbContext db) 
         {
             
             _db = db;
-
-
         }
 
         [HttpGet]
@@ -30,9 +27,9 @@ namespace MagicVilla_VillaAPI.Controllers
             return Ok(_db.Villas.ToList());
         }
         [HttpGet("{id:int}", Name = "GetVilla")]
-        [ProducesResponseType(200)]
-        [ProducesResponseType(404)]
-        [ProducesResponseType(400)]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
         public ActionResult<VillaDTO> GetVilla(int id)
         {
             if (id == 0)
@@ -44,23 +41,27 @@ namespace MagicVilla_VillaAPI.Controllers
             if (villa == null) {
                 return NotFound();
             }
-            return Ok();
+            //add in the villa to return the villa by id
+            return Ok(villa);
         }
         [HttpPost]
+        [ProducesResponseType(StatusCodes.Status201Created)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status500InternalServerError)]
         public ActionResult<VillaDTO> CreateVilla([FromBody] VillaDTO villaDTO)
         {
+
+            // Check if a villa with the same name already exists
+            if (_db.Villas.FirstOrDefault(u => u.Name.ToLower() == villaDTO.Name.ToLower()) != null)
+            {
+                ModelState.AddModelError("CustomError", "Villa already exists");
+                return BadRequest(ModelState);
+            }
+
             // Check if the villaDTO is null
             if (villaDTO == null)
             {
                 return BadRequest(villaDTO);
-            }
-
-            // Check if a villa with the same name already exists
-            //used StringComparison.OrdinalIgnoreCase instead of lower case
-            if (_db.Villas.FirstOrDefault(u => u.Name.Equals(villaDTO.Name, StringComparison.OrdinalIgnoreCase)) != null)
-            {
-                ModelState.AddModelError("CustomError", "Villa already exists");
-                return BadRequest(ModelState);
             }
 
             // Validate that the ID is not set
@@ -70,7 +71,7 @@ namespace MagicVilla_VillaAPI.Controllers
             }
 
             // Generate a new ID and add the villa to the list
-            _db.Villas.Add(_db.Villas.FirstOrDefault(u => u.Id == villaDTO.Id));
+            //_db.Villas.Add(_db.Villas.FirstOrDefault(u => u.Id == villaDTO.Id));
     
             Villa model = new()
             {
@@ -91,6 +92,10 @@ namespace MagicVilla_VillaAPI.Controllers
             return CreatedAtRoute("GetVilla", new { id = villaDTO.Id }, villaDTO);
         }
 
+        [ProducesResponseType(StatusCodes.Status204NoContent)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+
         [HttpDelete("{id:int}", Name = "DeleteVilla")]
         public ActionResult<VillaDTO> DeleteVilla(int id)
         {
@@ -104,8 +109,12 @@ namespace MagicVilla_VillaAPI.Controllers
                 return NotFound();
             }
             _db.Villas.Remove(villa);
+            //added save changes
+            _db.SaveChanges();
             return NoContent();
         }
+        [ProducesResponseType(StatusCodes.Status204NoContent)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
         [HttpPut("{id:int}", Name = "UpdateVilla")]
         public ActionResult UpdateVilla(int id, [FromBody] VillaDTO villaDTO)
         {
@@ -127,8 +136,9 @@ namespace MagicVilla_VillaAPI.Controllers
             };
             _db.Villas.Update(model);
             _db.SaveChanges();
+            return NoContent();
 
-            return CreatedAtRoute("GetVilla", new { id = villaDTO.Id }, villaDTO);
+            //return CreatedAtRoute("GetVilla", new { id = villaDTO.Id }, villaDTO);
 
         }
         [HttpPatch("{id:int}", Name = "UpdatePartialVilla")]
@@ -173,7 +183,7 @@ namespace MagicVilla_VillaAPI.Controllers
                 Sqft = villa.Sqft
 
             };
-            _db.Villas.Update(model);
+            _db.Villas.Update(villa);
             _db.SaveChanges();
             if (!ModelState.IsValid)
             {
